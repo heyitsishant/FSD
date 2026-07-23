77ac41cd9d6faabd23b9b4b85e7485c68b681208
heyitsishant <ishantkr7870@gmail.com>
2026-07-24 03:34:17 +0530

    Initial commit

---
+++ lecture-2/app.java
@@ -0,0 +1,9 @@
+public class app{
+    public static void main(String[] args) {
+        System.out.println("line 0: starting the program");
+        int x = 10;
+        int y = 2;
+        int z = x/y;
+        System.out.println("line 1:" + z);
+    }
+}
---
+++ lecture-2/app.py
@@ -0,0 +1,9 @@
+print("Line 0: hello world ")
+
+x = 10
+y = 2
+
+z  = x/y
+
+print(z)
+print("Line num 1:" + z)
